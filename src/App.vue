c7d11f06c6be5796644401341e291db9b00f5378
Elin Alsterbo <ealsterbo@gmail.com>
2020-02-27 13:22:41 +0100

    Lagt in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="logo">
+        <h2>Logo</h2>
+      </div>
+
+      <span id="logo" class="navbar-brand"></span>
+      <!--För att hamburger-menyn ska hamna till höger vid mindre skärm-->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Compare teams</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Compare players</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App"
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 80px;
+  width: 130px;
+  height: 100px;
+  background-color: darkgrey;
+}
+
+.navbar {
+  position: sticky;
+  top: 0;
+}
+
+.navbar-nav {
+  margin-left: 250px;
 }
 </style>
