f85f1b046465ca0418914acbd496a64bc5b7b2cf
Eliza <itzjusteliza@gmail.com>
2020-02-27 15:48:09 +0100

    views

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <News class="newstitle" />
+    <headercontent class="headercontainer" />
+  </div>
+</template>
+
+
+<script>
+import headercontent from "../components/headercontent.vue";
+import News from "../components/News.vue";
+
+export default {
+  name: "Home",
+  components: {
+    headercontent,
+    News
+  }
+};
+</script>
+
+<style></style>
