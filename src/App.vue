28e4fad703491e2c08c88f6ecc71c7fdb7db8381
Elin Alsterbo <ealsterbo@gmail.com>
2020-02-28 15:31:48 +0100

    Tagit bort det som gjorde att vi inte kunde köra live server

--- src/App.vue
+++ src/App.vue
@@ -39,9 +39,9 @@
     </nav>
     <router-view />
 
-    <button onclick="topFunction()" id="myBtn" title="Go to top">
+    <!-- <button onclick="topFunction()" id="myBtn" title="Go to top">
       <i class="fas fa-arrow-up"></i>
-    </button>
+    </button>-->
   </div>
 </template>
 
@@ -58,28 +58,6 @@ export default {
     // News
   }
 };
-
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 </script>
 
 <style>
@@ -101,43 +79,5 @@ function topFunction() {
 
 .nav-item {
   margin-right: 10px;
-}
-
-#myBtn {
-  display: none;
-  /* Hidden by default */
-  position: fixed;
-  /* Fixed/sticky position */
-  bottom: 20px;
-  /* Place the button at the bottom of the page */
-  right: 30px;
-  /* Place the button 30px from the right */
-  z-index: 99;
-  /* Make sure it does not overlap */
-  border: none;
-  /* Remove borders */
-  outline: none;
-  /* Remove outline */
-  background-color: rgba(211, 211, 211, 0.5);
-  /* Set a background color */
-  color: white;
-  /* Text color */
-  cursor: pointer;
-  /* Add a mouse pointer on hover */
-  padding: 0;
-  /* Some padding */
-  border-radius: 50%;
-  /* Rounded corners */
-  font-size: 18px;
-  /* Increase font size */
-}
-
-#myBtn:hover {
-  background-color: rgba(169, 169, 169, 0.5);
-  /* Add a dark-grey background on hover */
-}
-
-.fas {
-  padding: 15px;
 }
 </style>
