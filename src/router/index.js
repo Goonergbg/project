8500130362f7c65f6f00fae13981955a07f37afd
Elin Alsterbo <ealsterbo@gmail.com>
2020-03-02 13:49:07 +0100

    Lagt in komponenten för den andra jämföra-sidan

--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,25 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home'
 import Compare from "../components/Compare.vue"
+import comparePlayers from "../components/comparePlayers.vue"
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'headercontent',
     component: Home
   },
-{
-  path: '/compare-teams',
-  name: 'compare-teams',
-  component: Compare
-}
+  {
+    path: '/compare-teams',
+    name: 'compare-teams',
+    component: Compare
+  },
+  {
+    path: '/comparePlayers',
+    name: 'comparePlayers',
+    component: comparePlayers
+  }
 ]
 
 const router = new VueRouter({
